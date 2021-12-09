f6948e02810276e929364a8df16e669ee81dca37
dcreedon <damian.creedon@gmail.com>
2021-12-09 20:15:25 +0000

    make sure it works before demo :-) number 2

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@
 
 def serve():
     print("Hello World....")
-    print("Test for Jenkins Auto Build for Demo")
+    print("Test for Jenkins Auto Build for Demo  for praveen")
 
 if __name__ == '__main__':
     print("Launching Hello World...")
